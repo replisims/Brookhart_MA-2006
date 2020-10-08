3817f7d7eedb31be782f6e704c752defb4707893
KLuijken <k.luijken@lumc.nl>
2020-10-08 18:30:13 +0200

    Make output similar to original manuscript

--- Replication.Brookhart.2006/analysis/simulation/graphs.R
+++ Replication.Brookhart.2006/analysis/simulation/graphs.R
@@ -12,8 +12,6 @@
 #------------------------------------------------------------------------------#
 # Helper functions ----
 #------------------------------------------------------------------------------#
-
-variance <- function(x, rep) (1/(rep-1)) * sum((x - mean(x))^2)
 mse <- function(x, alpha4) mean((x - alpha4)^2)
 
 
@@ -21,32 +19,31 @@ mse <- function(x, alpha4) mean((x - alpha4)^2)
 # Figure 2, variance
 #------------------------------------------------------------------------------#
 
-create_figure2 <- function(){
-  output <- main_scenarios_2
-  output$alpha1 <- exp(output$alpha1)
-  output$beta1 <- exp(output$beta1)
+create_figure2 <- function(output){
+  output$alpha1 <- pnorm(output$alpha1*qnorm(.75))/pnorm(output$alpha1*qnorm(.25))
+  output$beta1 <- pnorm(output$beta1*qnorm(.75))/pnorm(output$beta1*qnorm(.25))
   output$variance_gamma_0 <- NA
   output$variance_gamma_1 <- NA
 
-  for(i in 1:nrow(main_scenarios_2)){
+  for(i in 1:nrow(output)){
     results <- readRDS(paste0("./analysis/data/raw_data/experiment_2_main/S",i,".rds"))
     results <- data.frame(results)
 
-    output[i,"variance_gamma_0"] <- variance(results[,"effect_crude"], rep = 1000)
-    output[i,"variance_gamma_1"] <- variance(results[,"effect_splines"], rep = 1000)
+    output[i,"variance_gamma_0"] <- var(results[,"effect_crude"])
+    output[i,"variance_gamma_1"] <- var(results[,"effect_splines"])
   }
 
 
-  input_plot <- matrix(NA, nrow = length(beta1), ncol = 5)
+  input_plot <- matrix(NA, nrow = length(unique(output$beta1)), ncol = 5)
   colnames(input_plot) <- c("beta1","variance_gamma_0_n500","variance_gamma_0_n2500","variance_gamma_1_n500","variance_gamma_1_n2500")
 
   input_plot[,"beta1"] <- pnorm(beta1*qnorm(.75))/pnorm(beta1*qnorm(.25))
 
-  for(j in 1:length(beta1)){
-    input_plot[j,"variance_gamma_0_n500"] <- mean(output[output[,"nobs"]==500 & output[,"beta1"] == exp(beta1[j]),"variance_gamma_0"])
-    input_plot[j,"variance_gamma_1_n500"] <- mean(output[output[,"nobs"]==500 & output[,"beta1"] == exp(beta1[j]),"variance_gamma_1"])
-    input_plot[j,"variance_gamma_0_n2500"] <- mean(output[output[,"nobs"]==2500 & output[,"beta1"] == exp(beta1[j]),"variance_gamma_0"])
-    input_plot[j,"variance_gamma_1_n2500"] <- mean(output[output[,"nobs"]==2500 & output[,"beta1"] == exp(beta1[j]),"variance_gamma_1"])
+  for(j in 1:length(unique(output$beta1))){
+    input_plot[j,"variance_gamma_0_n500"] <- mean(output[output[,"nobs"]==500 & output[,"beta1"] == input_plot[j,"beta1"],"variance_gamma_0"])
+    input_plot[j,"variance_gamma_1_n500"] <- mean(output[output[,"nobs"]==500 & output[,"beta1"] == input_plot[j,"beta1"],"variance_gamma_1"])
+    input_plot[j,"variance_gamma_0_n2500"] <- mean(output[output[,"nobs"]==2500 & output[,"beta1"] == input_plot[j,"beta1"],"variance_gamma_0"])
+    input_plot[j,"variance_gamma_1_n2500"] <- mean(output[output[,"nobs"]==2500 & output[,"beta1"] == input_plot[j,"beta1"],"variance_gamma_1"])
 
   }
 
@@ -55,7 +52,7 @@ create_figure2 <- function(){
        y = input_plot[,"variance_gamma_0_n500"],
        type = "l",
        lty = 4,
-       xlim=c(min(exp(beta1)),max(exp(beta1))),
+       xlim = c(min(unique(output$beta1)), 3.1),
        ylim = c(0,0.007),
        xlab = "Association between X1 and A (relative risk)",
        ylab = "Variance of estimator")
@@ -71,53 +68,56 @@ create_figure2 <- function(){
 # Figure 3, contour plot splines
 #------------------------------------------------------------------------------#
 
-create_figure3 <- function(){
-  output <- main_scenarios_2
-  output$alpha1 <- exp(output$alpha1)
-  output$beta1 <- exp(output$beta1)
+create_figure3 <- function(output, alpha4){
+  output$alpha1 <- pnorm(output$alpha1*qnorm(.75))/pnorm(output$alpha1*qnorm(.25))
+  output$beta1 <- pnorm(output$beta1*qnorm(.75))/pnorm(output$beta1*qnorm(.25))
   output$mse_gamma_0 <- NA
   output$mse_gamma_1 <- NA
 
-  for(i in 1:nrow(main_scenarios_2)){
+  for(i in 1:nrow(output)){
     results <- readRDS(paste0("./analysis/data/raw_data/experiment_2_main/S",i,".rds"))
     results <- data.frame(results)
 
-    output[i,"mse_gamma_0"] <- mse(results[,"effect_crude"])
-    output[i,"mse_gamma_1"] <- mse(results[,"effect_splines"])
+    output[i,"mse_gamma_0"] <- mse(results[,"effect_crude"], alpha4 = alpha4)
+    output[i,"mse_gamma_1"] <- mse(results[,"effect_splines"], alpha4 = alpha4)
   }
 
   ### n = 500
 
   output_n500    <- output[output[,"nobs"] == 500,]
   mse_ratio_n500 <- output_n500[,"mse_gamma_1"]/output_n500[,"mse_gamma_0"]
-  output_n500    <- cbind(output_n500[,c("alpha1","beta1")],mse_ratio_n500)
+  output_n500    <- cbind(output_n500[,c("alpha1","beta1")], mse_ratio_n500)
 
   mse_loess_n500    <- loess(mse_ratio_n500 ~ alpha1 * beta1, data = output_n500)
-  grid_contour_n500 <- expand.grid(alpha1 = exp(alpha1), beta1= exp(beta1))
+  grid_contour_n500 <- expand.grid(alpha1 = pnorm(alpha1*qnorm(.75))/pnorm(alpha1*qnorm(.25)),
+                                   beta1 = pnorm(beta1*qnorm(.75))/pnorm(beta1*qnorm(.25)))
   input_contour_n500<- predict(mse_loess_n500, newdata = grid_contour_n500)
 
   ### n = 2,500
 
   output_n2500    <- output[output[,"nobs"] == 2500,]
   mse_ratio_n2500 <- output_n2500[,"mse_gamma_1"]/output_n2500[,"mse_gamma_0"]
-  output_n2500    <- cbind(output_n2500[,c("alpha1","beta1")],mse_ratio_n2500)
+  output_n2500    <- cbind(output_n2500[,c("alpha1","beta1")], mse_ratio_n2500)
 
   mse_loess_n2500    <- loess(mse_ratio_n2500 ~ alpha1 * beta1, data = output_n2500)
-  grid_contour_n2500 <- expand.grid(alpha1 = exp(alpha1), beta1= exp(beta1))
+  grid_contour_n2500 <- expand.grid(alpha1 = pnorm(alpha1*qnorm(.75))/pnorm(alpha1*qnorm(.25)),
+                                    beta1 = pnorm(beta1*qnorm(.75))/pnorm(beta1*qnorm(.25)))
   input_contour_n2500<- predict(mse_loess_n2500, newdata = grid_contour_n2500)
 
   png("./analysis/figures/Figure3.png",width = 480, height = 480)
-  par(oma=c(3,3,3,3))
-  par(mar=c(0,0,0,0))
-  par(mfrow=c(1,2))
-  contour(x = exp(alpha1), y = exp(beta1), z = input_contour_n500, levels = c(0.5,0.9,1.1,1.5),axes = FALSE)
+  par(oma = c(3,3,3,3))
+  par(mar = c(0,0,0,0))
+  par(mfrow = c(1,2))
+  contour(x = unique(output$alpha1), y = unique(output$beta1),
+          z = input_contour_n500, levels = c(0.5,0.9,1.1,1.5),axes = FALSE, ylim = c(1,3.5))
   abline(h = 3.5)
   axis(1)
   axis(2)
   axis(3, labels = F)
   axis(4, labels = F, tick = F)
   mtext("n = 500", side=3, line=-0.7, adj=0.5, cex=0.7)
-  contour(x = exp(alpha1), y = exp(beta1), z = input_contour_n2500, levels = c(0.5,0.9,1.1,1.5), axes = FALSE)
+  contour(x = unique(output$alpha1), y = unique(output$beta1),
+          z = input_contour_n2500, levels = c(0.5,0.9,1.1,1.5), axes = FALSE, ylim = c(1,3.5))
   abline(v = c(1,3.5))
   abline(h = 3.5)
   axis(1, labels = F)
@@ -134,19 +134,18 @@ create_figure3 <- function(){
 # Figure 4, contour plot quintiles
 #------------------------------------------------------------------------------#
 
-create_figure4 <- function(){
-  output <- main_scenarios_2
-  output$alpha1 <- exp(output$alpha1)
-  output$beta1 <- exp(output$beta1)
+create_figure4 <- function(output, alpha4){
+  output$alpha1 <- pnorm(output$alpha1*qnorm(.75))/pnorm(output$alpha1*qnorm(.25))
+  output$beta1 <- pnorm(output$beta1*qnorm(.75))/pnorm(output$beta1*qnorm(.25))
   output$mse_gamma_0 <- NA
   output$mse_gamma_1 <- NA
 
-  for(i in 1:nrow(main_scenarios_2)){
+  for(i in 1:nrow(scenarios_2_main)){
     results <- readRDS(paste0("./analysis/data/raw_data/experiment_2_main/S",i,".rds"))
     results <- data.frame(results)
 
-    output[i,"mse_gamma_0"] <- mse(results[,"effect_crude"])
-    output[i,"mse_gamma_1"] <- mse(results[,"effect_quintiles"])
+    output[i,"mse_gamma_0"] <- mse(results[,"effect_crude"], alpha4 = alpha4)
+    output[i,"mse_gamma_1"] <- mse(results[,"effect_quintiles"], alpha4 = alpha4)
   }
 
   ### n = 500
@@ -156,7 +155,8 @@ create_figure4 <- function(){
   output_n500    <- cbind(output_n500[,c("alpha1","beta1")],mse_ratio_n500)
 
   mse_loess_n500    <- loess(mse_ratio_n500 ~ alpha1 * beta1, data = output_n500)
-  grid_contour_n500 <- expand.grid(alpha1 = exp(alpha1), beta1= exp(beta1))
+  grid_contour_n500 <- expand.grid(alpha1 = pnorm(alpha1*qnorm(.75))/pnorm(alpha1*qnorm(.25)),
+                                   beta1 = pnorm(beta1*qnorm(.75))/pnorm(beta1*qnorm(.25)))
   input_contour_n500<- predict(mse_loess_n500, newdata = grid_contour_n500)
 
   ### n = 2,500
@@ -166,21 +166,24 @@ create_figure4 <- function(){
   output_n2500    <- cbind(output_n2500[,c("alpha1","beta1")],mse_ratio_n2500)
 
   mse_loess_n2500    <- loess(mse_ratio_n2500 ~ alpha1 * beta1, data = output_n2500)
-  grid_contour_n2500 <- expand.grid(alpha1 = exp(alpha1), beta1= exp(beta1))
+  grid_contour_n2500 <- expand.grid(alpha1 = pnorm(alpha1*qnorm(.75))/pnorm(alpha1*qnorm(.25)),
+                                   beta1 = pnorm(beta1*qnorm(.75))/pnorm(beta1*qnorm(.25)))
   input_contour_n2500<- predict(mse_loess_n2500, newdata = grid_contour_n2500)
 
   png("./analysis/figures/Figure4.png",width = 480, height = 480)
-  par(oma=c(3,3,3,3))
-  par(mar=c(0,0,0,0))
-  par(mfrow=c(1,2))
-  contour(x = exp(alpha1), y = exp(beta1), z = input_contour_n500, levels = c(0.5,0.9,1.1,1.5),axes = FALSE)
+  par(oma = c(3,3,3,3))
+  par(mar = c(0,0,0,0))
+  par(mfrow = c(1,2))
+  contour(x = unique(output$alpha1), y = unique(output$beta1), ylim = c(1,3.5),
+          z = input_contour_n500, levels = c(0.5,0.9,1.1,1.5),axes = FALSE)
   abline(h = 3.5)
   axis(1)
   axis(2)
   axis(3, labels = F)
   axis(4, labels = F, tick = F)
   mtext("n = 500", side=3, line=-0.7, adj=0.5, cex=0.7)
-  contour(x = exp(alpha1), y = exp(beta1), z = input_contour_n2500, levels = c(0.5,0.9,1.1,1.5), axes = FALSE)
+  contour(x = unique(output$alpha1), y = unique(output$beta1), ylim = c(1,3.5),
+          z = input_contour_n2500, levels = c(0.5,0.9,1.1,1.5), axes = FALSE)
   abline(v = c(1,3.5))
   abline(h = 3.5)
   axis(1, labels = F)
--- Replication.Brookhart.2006/analysis/simulation/tables.R
+++ Replication.Brookhart.2006/analysis/simulation/tables.R
@@ -12,31 +12,33 @@
 #------------------------------------------------------------------------------#
 # Helper functions ----
 #------------------------------------------------------------------------------#
-bias     <- function(x) mean(x - alpha4)
-variance <- function(x) (1/(rep-1)) * sum((x - mean(x))^2)
-mse      <- function(x) mean((x - alpha4)^2)
+bias     <- function(x, alpha4) mean(x - alpha4)
+mse      <- function(x, alpha4) mean((x - alpha4)^2)
 
 
 single_scenario <- function(filepath,
                             output_effect,
                             ps_covs_1 = ps_covs_1,
-                            multiplication){
+                            multiplication,
+                            alpha4 = alpha4){
   results <- readRDS(paste0("./analysis/data/raw_data/", filepath))
   results <- data.frame(results)
 
   # Bias
-  output_bias   <- rbind(by(results[,output_effect], results[,"ps_model"], function(x) bias(x) * multiplication))
-  output_bias   <- cbind(output_bias, bias(results[,"effect_crude"]) * multiplication)
+  output_bias   <- rbind(by(results[,output_effect], results[,"ps_model"], function(x, alpha4) bias(x, alpha4 = alpha4) * multiplication, alpha4))
+  output_bias   <- cbind(output_bias, bias(results[,"effect_crude"], alpha4 = alpha4) * multiplication)
 
   # Variance
-  output_var    <- rbind(by(results[,output_effect], results[,"ps_model"], function(x) variance(x) * multiplication))
-  output_var    <- cbind(output_var, variance(results[,"effect_crude"]) * multiplication)
+  output_var    <- rbind(by(results[,output_effect], results[,"ps_model"], function(x) var(x) * multiplication))
+  output_var    <- cbind(output_var, var(results[,"effect_crude"]) * multiplication)
 
   # MSE
-  output_mse    <- rbind(by(results[,output_effect], results[,"ps_model"], function(x) mse(x) * multiplication))
-  output_mse    <- cbind(output_mse, mse(results[,"effect_crude"]) * multiplication)
+  output_mse    <- rbind(by(results[,output_effect], results[,"ps_model"], function(x, alpha4) mse(x, alpha4 = alpha4) * multiplication, alpha4))
+  output_mse    <- cbind(output_mse, mse(results[,"effect_crude"], alpha4 = alpha4) * multiplication)
 
   output        <- rbind(output_bias, output_var, output_mse)
+  colnames(output) <- c("X1", "X1+X2", "X1+X2+X3", "X1+X3", "X2", "X2+X3", "X3", "None")
+  output        <- output[,c("X1","X2","X3","X1+X2","X1+X3","X2+X3","X1+X2+X3","None")]
 
   return(output)
 }
@@ -47,6 +49,7 @@ add_c_stat <- function(filepath){
   results <- data.frame(results)
 
   c_stat <- rbind(by(results[,"c_stat"], results[,"ps_model"], function(x) mean(x)))
+  c_stat <- c_stat[,c("X1","X2","X3","X1+X2","X1+X3","X2+X3","X1+X2+X3")]
   return(c_stat)
 }
 
@@ -59,13 +62,15 @@ create_table1 <- function(){
                         single_scenario(filepath = "experiment_1_main/S1.rds",
                                         output_effect = "effect_splines",
                                         ps_covs_1 = ps_covs_1,
-                                        multiplication = 10),
+                                        multiplication = 10,
+                                        alpha4 = alpha4),
                         c(add_c_stat(filepath = "experiment_1_main/S1.rds"),NA),
                         rep(NA, times = 8),
                         single_scenario(filepath = "experiment_1_main/S2.rds",
                                         output_effect = "effect_splines",
                                         ps_covs_1 = ps_covs_1,
-                                        multiplication = 100),
+                                        multiplication = 100,
+                                        alpha4 = alpha4),
                         c(add_c_stat(filepath = "experiment_1_main/S2.rds"),NA)),digits = 2)
   rownames(table1) <- c("n = 500","Bias x 10","Variance x 10","MSE x 10","Average c-statistic",
                         "n = 2,500","Bias x 100","Variance x 100","MSE x 100","Average cstatistic")
@@ -73,11 +78,12 @@ create_table1 <- function(){
   addtorow <- list()
   addtorow$pos <- list(0,0)
   addtorow$command <- c("& \\multicolumn{8}{c}{Variable(s) in propensity score model} \\\\\n",
-                        "& X1 & X1+X2 & X1+X2+X3 & X1+X3 & X2 & X2+X3 & X3 & None \\\\\n")
+                        "& X1 & X2 & X3 & X1+X2 & X1+X3 & X2+X3 & X1+X2+X3 & None \\\\\n")
 
   print(table1, add.to.row = addtorow, include.colnames = FALSE)
 }
-  #------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
 # Table 2: Experiment 1, subclassification
 #------------------------------------------------------------------------------#
 
@@ -86,19 +92,21 @@ create_table2 <- function(){
                          single_scenario(filepath = "experiment_1_main/S1.rds",
                                          output_effect = "effect_quintiles",
                                          ps_covs_1 = ps_covs_1,
-                                         multiplication = 10),
+                                         multiplication = 10,
+                                         alpha4 = alpha4),
                          rep(NA, times = 8),
                          single_scenario(filepath = "experiment_1_main/S2.rds",
                                          output_effect = "effect_quintiles",
                                          ps_covs_1 = ps_covs_1,
-                                         multiplication = 100)),digits = 2)
+                                         multiplication = 100,
+                                         alpha4 = alpha4)),digits = 2)
   rownames(table2) <- c("n = 500","Bias x 10","Variance x 10","MSE x 10",
                         "n = 2,500","Bias x 100","Variance x 100","MSE x 100")
 
   addtorow <- list()
   addtorow$pos <- list(0,0)
   addtorow$command <- c("& \\multicolumn{8}{c}{Variable(s) in propensity score model} \\\\\n",
-                        "& X1 & X1+X2 & X1+X2+X3 & X1+X3 & X2 & X2+X3 & X3 & None \\\\\n")
+                        "& X1 & X2 & X3 & X1+X2 & X1+X3 & X2+X3 & X1+X2+X3 & None \\\\\n")
 
   print(table2, add.to.row = addtorow, include.colnames = FALSE)
 }
@@ -112,7 +120,8 @@ create_table3 <- function(){
                   single_scenario(filepath = "experiment_1_main/S1.rds",
                                   output_effect = "effect_splines",
                                   ps_covs_1 = ps_covs_1,
-                                  multiplication = 10))
+                                  multiplication = 10,
+                                  alpha4 = alpha4))
 
   for(i in 1:nrow(sensitivity_1)){
     table3 <- rbind(table3,
@@ -120,7 +129,8 @@ create_table3 <- function(){
                     single_scenario(filepath = paste0("experiment_1_sensitivity/S",i,".rds"),
                                     output_effect = "effect_splines",
                                     ps_covs_1 = ps_covs_1,
-                                    multiplication = 10))
+                                    multiplication = 10,
+                                    alpha4 = sensitivity_1$alpha4[i]))
   }
   table3 <- round(table3, digits = 2)
 
@@ -140,7 +150,7 @@ create_table3 <- function(){
   addtorow <- list()
   addtorow$pos <- list(0,0)
   addtorow$command <- c("& \\multicolumn{8}{c}{Variable(s) in propensity score model} \\\\\n",
-                        "& X1 & X1+X2 & X1+X2+X3 & X1+X3 & X2 & X2+X3 & X3 & None \\\\\n")
+                        "& X1 & X2 & X3 & X1+X2 & X1+X3 & X2+X3 & X1+X2+X3 & None \\\\\n")
 
   print(table3, add.to.row = addtorow, include.colnames = FALSE)
 }
